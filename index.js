b76b0a12796f57274bfdd4f76f51115e5e3cf427
Harshit Goswami <harshit@biz4group.com>
2024-07-29 12:48:53 +0530

    index file chanes

--- index.js
+++ index.js
@@ -26,9 +26,10 @@ function modulus(a, b) {
     return a % b;
 }
 
-// Example usage
-console.log("Addition: 5 + 3 =", add(5, 3));          // Output: 8
-console.log("Subtraction: 5 - 3 =", subtract(5, 3));  // Output: 2
-console.log("Multiplication: 5 * 3 =", multiply(5, 3)); // Output: 15
-console.log("Division: 5 / 3 =", divide(5, 3));       // Output: 1.6666666666666667
-console.log("Modulus: 5 % 3 =", modulus(5, 3));       // Output: 2
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+    modulus
+}
